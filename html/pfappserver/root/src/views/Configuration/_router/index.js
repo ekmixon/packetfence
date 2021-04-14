a013b43d30e463eb9b70b311717eeb8502a75859
Durand Fabrice <oeufdure@gmail.com>
2021-04-14 11:01:07 -0400

    Recover ztna

--- html/pfappserver/root/src/views/Configuration/_router/index.js
+++ html/pfappserver/root/src/views/Configuration/_router/index.js
@@ -10,6 +10,7 @@ import SourcesRoutes from '../sources/_router'
 import SwitchesRoutes from '../switches/_router'
 import SwitchGroupsRoutes from '../switchGroups/_router'
 import ConnectionProfilesRoutes from '../connectionProfiles/_router'
+import RemoteConnectionProfilesRoutes from '../remoteConnectionProfiles/_router'
 
 /* Compliance */
 const ComplianceSection = () => import(/* webpackChunkName: "Configuration" */ '../_components/ComplianceSection')
@@ -95,6 +96,7 @@ const route = {
     ...SwitchesRoutes,
     ...SwitchGroupsRoutes,
     ...ConnectionProfilesRoutes,
+    ...RemoteConnectionProfilesRoutes,
 
     /**
      * Compliance
